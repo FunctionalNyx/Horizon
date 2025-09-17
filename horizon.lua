72e4a6634fd63aa7fa7375a19ebfc7b43eabf5ed
Nyx <aduckboi@gmail.com>
2025-09-17 12:18:21 -0700

    Blessing works properly now

--- horizon.lua
+++ horizon.lua
@@ -6528,7 +6528,7 @@ SMODS.Consumable {
 		end
     end,
 	can_use = function(self, card)
-        return #G.jokers.highlighted
+         return #G.jokers.highlighted == 1
     end
 }
 SMODS.Consumable {
