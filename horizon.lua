f58c48d96401c8b99f402f59205561cf24fd0000
Nyx <aduckboi@gmail.com>
2025-12-02 16:33:10 -0800

    Why was this hidden

--- horizon.lua
+++ horizon.lua
@@ -10036,7 +10036,6 @@ SMODS.Tag {
     key = "hnhtag",
     min_ante = 2,
     atlas = 'Tags',
-	no_collection = true,
     pos = { x = 2, y = 0 },
     loc_vars = function(self, info_queue, tag)
         info_queue[#info_queue + 1] = G.P_CENTERS.p_nyx_hnh_pack
